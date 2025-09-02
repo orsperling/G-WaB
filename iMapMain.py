926c0d9b7fb1d340cd2b402bcd31621bd03fb46b
Your NameOrSp <orsp@agri.gov.il>
2025-09-02 10:33:38 +0300

    test

--- iMapMain.py
+++ iMapMain.py
@@ -234,7 +234,7 @@ def save_map_as_image_static(lat, lon, zoom=15, size=(600, 450), marker_path='im
 # 🌟 **Streamlit UI**
 st.markdown("<h1 style='text-align: center;'>ALMOND - irrigation Monthly Annual Planner</h1>", unsafe_allow_html=True)
 st.markdown(
-    "<p style='text-align: center; font-size: 20px'>This is a research report founded by <a href=\"https://www.bard-isus.org/\"> <strong>BARD</strong></a>. </p>",
+    "<p style='text-align: center; font-size: 20px'>This is a research report founded by <a href=\"https://www.bard-isus.org/\"> <strong>BAARD</strong></a>. </p>",
     unsafe_allow_html=True)
 st.markdown(
     "<p style='text-align: center;'>For further information contact: <a href=\"mailto:orsp@volcani.agri.gov.il\"> <strong>Or Sperling</strong></a> (ARO-Volcani), <a href=\"mailto:mzwienie@ucdavis.edu\"> <strong>Maciej Zwieniecki</strong></a> (UC Davis), <a href=\"mailto:zellis@ucdavis.edu\"> <strong>Zac Ellis</strong></a> (UC Davis), <a href=\"mailto:niccolo.tricerri@unito.it\"> <strong>Niccolò Tricerri</strong></a> (UNITO - IUSS Pavia)  </p>",
